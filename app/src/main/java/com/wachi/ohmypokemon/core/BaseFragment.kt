f643df81f9c16146f55b4a3eb459f9085f3365ae
w.prasertwong <w.prasertwong@accenture.com>
2021-01-11 23:04:48 +0700

    add base fragment, base view model

---
+++ app/src/main/java/com/wachi/ohmypokemon/core/BaseFragment.kt
@@ -0,0 +1,11 @@
+package com.wachi.ohmypokemon.core
+
+import androidx.fragment.app.Fragment
+import com.wachi.ohmypokemon.core.dialog.Dialog
+import org.koin.android.ext.android.inject
+import org.koin.core.parameter.parametersOf
+
+open class BaseFragment: Fragment() {
+
+    protected val dialog: Dialog by inject { parametersOf(this) }
+}
